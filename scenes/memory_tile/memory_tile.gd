81bac901e2b14b3a82c3f84b1c186bf19dbd9075
Nils Nahooy <nils.nahooy@live.com>
2024-07-31 17:35:07 +0200

    add setup

--- scenes/memory_tile/memory_tile.gd
+++ scenes/memory_tile/memory_tile.gd
@@ -3,6 +3,8 @@ extends TextureButton
 @onready var frame_image: TextureRect = $FrameImage
 @onready var item_image: TextureRect = $ItemImage
 
+var _item_name: String
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,3 +13,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func get_item_name() -> String:
+	return _item_name
+
+func setup(ii_dict: Dictionary, fi: CompressedTexture2D) -> void:
+	fi.texture = fi
+	item_image.texture = ii_dict.item_texture
+	_item_name = ii_dict.item_name
