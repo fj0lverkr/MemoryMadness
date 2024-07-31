c98228dce344cc77e367ef68f284c6188bf7bc1f
Nils Nahooy <nils.nahooy@live.com>
2024-07-31 19:49:46 +0200

    Make tiles clickable

--- autoloads/signal_bus.gd
+++ autoloads/signal_bus.gd
@@ -2,3 +2,5 @@ extends Node
 
 signal on_level_selected(level: int)
 signal on_game_exit_pressed
+signal on_select_enabled
+signal on_select_disabled
--- scenes/memory_tile/memory_tile.gd
+++ scenes/memory_tile/memory_tile.gd
@@ -4,20 +4,31 @@ extends TextureButton
 @onready var item_image: TextureRect = $ItemImage
 
 var _item_name: String
+var can_select_me: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	SignalBus.on_select_disabled.connect(_on_select_disabled)
+	SignalBus.on_select_enabled.connect(_on_select_enabled)
 
+func _on_select_disabled() -> void:
+	can_select_me = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _on_select_enabled() -> void:
+	can_select_me = true
 
 func get_item_name() -> String:
 	return _item_name
 
+func toggle_reveal(r: bool) -> void:
+	item_image.visible = r
+	frame_image.visible = r
+
 func setup(ii_dict: Dictionary, fi: CompressedTexture2D) -> void:
+	toggle_reveal(false)
 	frame_image.texture = fi
 	item_image.texture = ii_dict.item_texture
 	_item_name = ii_dict.item_name
+
+func _on_pressed() -> void:
+	toggle_reveal(true)
