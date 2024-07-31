2c6348f691feacc3400bbc37ec413c356633d593
Nils Nahooy <nils.nahooy@live.com>
2024-07-31 19:36:08 +0200

    Cleanup

--- scenes/level_button/level_button.gd
+++ scenes/level_button/level_button.gd
@@ -5,15 +5,10 @@ extends TextureButton
 
 var _level_number: int = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
 func set_level_number(level_num: int) -> void:
 	_level_number = level_num
 	var level_data: Dictionary = GameManager.LEVELS[_level_number]
 	level_label.text = "%sx%s" % [level_data.rows, level_data.cols]
-
 
 func _on_pressed() -> void:
 	AudioManager.play_button_clicked(button_audio_player)
